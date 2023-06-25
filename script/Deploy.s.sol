a5ede7bd545a6ecec3ce8f4597e06a60bc6fc006
nandorcsupor <csuprok@hotmail.com>
2023-06-25 16:05:18 +0200

    all

---
+++ script/Deploy.s.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MyToken} from "../src/MyToken.sol";
+
+contract Deploy is Script {
+    uint256 public constant INITIAL_SUPPLY = 1000 ether;
+
+    function run() external {
+        vm.startBroadcast();
+        new MyToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+    }
+}
---
+++ src/MyToken.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+        _mint(msg.sender, initialSupply);
+    }
+}
